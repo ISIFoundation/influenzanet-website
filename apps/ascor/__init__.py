2ec6f8d22905b9632ffc10f8d147adf309679d06
Herve <corentin.herve@iplesp.upmc.fr>
2019-05-23 16:06:16 +0200

    removed apps ascor (buging)

--- apps/ascor/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-if not hasattr(settings, 'ASCOR_RSA_PRIVATE_KEY'):
-    raise Exception("Ascor config: missing private key configuration")
-
-if not hasattr(settings, 'ASCOR_RSA_PUBLIC_KEY'):
-    raise Exception("Ascor config: missing public key configuration")
-
-ASCOR_DEBUG = getattr(settings, 'ASCOR_DEBUG', False)
--- apps/ascor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-from . import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='ascor_index'),
-)
